33a6235c93cdcc04c67520fdcb4285265be21721
thuryaaldj <thuryaamen@gmail.com>
2025-08-14 11:28:10 +0300

    Posts Table

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/postsTable";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
@@ -26,6 +27,7 @@ export default function Home() {
       icon={<MessageCircle className="text-slate-500" size={72}/>} />
 
     </div>
+    <PostsTable title="Latest Posts"/>
     
   </>
   );
---
+++ components/posts/postsTable.tsx
@@ -0,0 +1,54 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+import Link from 'next/link'
+import { Post } from '@/types/posts';
+import posts from '@/data/posts';
+
+interface PostsTableProps{
+    limit?:number;
+    title?:string;
+}
+
+const PostsTable = ({limit,title}:PostsTableProps) => {
+    return <div className='mt-10'>
+        <h3 className='text-2xl mb-4 font-semibold'>
+            { title ? title :'posts'}
+        </h3>
+        <Table>
+            <TableCaption>A list of reacent posts</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Title</TableHead>
+                        <TableHead className='hidden md:table-cell'>Author</TableHead>
+                        <TableHead className='hidden md:table-cell text-right'>Date</TableHead>
+                        <TableHead>View</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {posts.map((post)=>(
+                        <TableRow key={post.id} >
+                            <TableCell>{post.title}</TableCell>
+                            <TableCell className='hidden md:table-cell'>{post.author}</TableCell>
+                            <TableCell className='text-right hidden md:table-cell'>e{post.date}</TableCell>
+                        <TableCell>
+                            <Link href={`/posts/edit/${post.id}`} >
+                            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs'>
+                                Edit
+                            </button>
+                            </Link>
+                        </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+        </Table>
+    </div>;
+}
+
+export default PostsTable;
